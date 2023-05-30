be05d54a3bda6b553bbb51f11bac88ce936f4089
erbester51 <55757768+erbester51@users.noreply.github.com>
2023-05-30 15:53:55 -0500

    Added Middleware

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const path = require('path');
-const dataBase = require('db');
+const notesData = require('db');
+const app = express();
 const PORT = 3001;
 
+// Middleware
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
