f3149adcf14d26bd03333176e29d6c2b95508516
erbester51 <55757768+erbester51@users.noreply.github.com>
2023-07-18 09:01:42 -0500

    Updated GET and POST routes

--- Develop/server.js
+++ Develop/server.js
@@ -10,9 +10,28 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.post
+// GET
+app.get('/notes', (req, res) => {
+    res.sendFile(__dirname + "/public/notes.html");
+  });
+  
+  app.get('/api/notes', (req, res) => res.json(notesData));
+  
+  app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
+  });
+
+app.post('/api/notes', req, res) => {
+    const { title, text } = req.body;
+    const id = uuidv4();
+
+    const newData = {
+        title,
+        text, 
+        id
+    }
+}
 
 // Bonus
 app.delete
 
-app. get
